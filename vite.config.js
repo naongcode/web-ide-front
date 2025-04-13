70f6bff2eb7d3e7d049a45226554dc347caeddfa
yujin <yujin09219@naver.com>
2025-04-14 07:44:19 +0900

    chore: storybook 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js,jsx,tsx}"];
+export const theme = {
+  extend: {
+    colors: {
+      base: '#2D336B', // 기본
+      base2: '#7886C7', // 기본 2
+      base3: '#A9B5DF', // 기본 3
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+      error: '#C13515', // 에러 1
+      error2: '#FEF8F6', // 에러 2
+
+      transparent1: '#9099AC', // 투명1
+      transparent2: '#9099ACB8', // 투명2 (47% → B8)
+      transparent3: '#7E8798', // 투명3
+
+      gray1: '#E7E7E7', // 회색조1
+      gray2: '#B0B0B0', // 회색조2
+      gray3: '#717171', // 회색조3
+
+      code: '#1F2228', // 코드
+      white: '#000000', // 흰색
+    },
+  },
+};
+export const plugins = [];
